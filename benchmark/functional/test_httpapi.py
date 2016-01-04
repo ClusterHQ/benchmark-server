386853ff83e89639f75f260ab6acb95a5fbaf200
Andriy Gapon <avg@icyb.net.ua>
2016-01-04 13:43:59 +0200

    add functional tests for TxMongoBackend based on BenchmarkAPITestsMixin

---
+++ benchmark/functional/test_httpapi.py
@@ -0,0 +1,15 @@
+from testtools import TestCase
+
+from ..httpapi import TxMongoBackend
+from ..test.test_httpapi import BenchmarkAPITestsMixin
+
+
+class TxMongoBenchmarkAPITests(BenchmarkAPITestsMixin, TestCase):
+    def setUp(self):
+        self.backend = TxMongoBackend()
+        self.addCleanup(self.backend.disconnect)
+        super(TxMongoBenchmarkAPITests, self).setUp()
+
+    def tearDown(self):
+        super(TxMongoBenchmarkAPITests, self).tearDown()
+        return self.backend.collection.delete_many({})
